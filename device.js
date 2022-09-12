8387a8b4755bb715fa5e88a3039c87d6886b87e6
Rohan Buch <buchrohand2@gmail.com>
2022-09-12 13:52:01 -0700

    more changes

--- device.js
+++ device.js
@@ -12,11 +12,12 @@ var engine;
     })();
     engine.cameraEngine = cameraEngine;
     var Mesh = (function () {
-        function Mesh(name, verticesCount) {
+        function Mesh(name, verticesCount, facesCount) {
             this.name = name;
             this.Vertices = new Array(verticesCount);
-            this.Rotation = BABYLON.Vector3.Zero();
-            this.Position = BABYLON.Vector3.Zero();
+            this.Faces = new Array(facesCount);
+            this.Rotation = BABYLONTS.Vector3(0, 0, 0);
+            this.Position = BABYLONTS.Vector3(0, 0, 0);
         }
         return Mesh;
     })();
@@ -64,11 +65,55 @@ var engine;
                 } //above, draw a yellow point
         };
 
+        Device.prototype.drawLine = function (point0, point1) {
+            var dist = point1.subtract(point0).length();
+        
+            // If the distance between the 2 points is less than 2 pixels
+            // We're exiting
+            if(dist < 2) {
+                return;
+            }
+        
+            // Find the middle point between first & second point
+            var middlePoint = point0.add((point1.subtract(point0)).scale(0.5));
+            // We draw this point on screen
+            this.drawPoint(middlePoint);
+            // Recursive algorithm launched between first & middle point
+            // and between middle & second point
+            this.drawLine(point0, middlePoint);
+            this.drawLine(middlePoint, point1);
+        };
+
+        Device.prototype.drawBline = function (point0, point1) { //bresehan algorithm
+            var x0 = point0.x >> 0;
+            var y0 = point0.y >> 0;
+            var x1 = point1.x >> 0;
+            var y1 = point1.y >> 0;
+            var dx = Math.abs(x1 - x0);
+            var dy = Math.abs(y1 - y0);
+            var sx = (x0 < x1) ? 1 : -1;
+            var sy = (y0 < y1) ? 1 : -1;
+            var err = dx - dy;
+            while(true) {
+                this.drawPoint(new BABYLON.Vector2(x0, y0));
+                if((x0 == x1) && (y0 == y1)) break;
+                var e2 = 2 * err;
+                if(e2 > -dy) { err -= dy; x0 += sx; }
+                if(e2 < dx) { err += dx; y0 += sy; }
+            }
+        };
+
         device.prototype.render = function (camera, meshes) { //recompute each proj
             // projection matrix equation
             var viewMatrix = BABYLON.Matrix.LookAtLH(camera.Position, camera.Target, BABYLON.Vector3.Up());
             var projectionMatrix = BABYLON.Matrix.PerspectiveFovLH(0.78, 
                                            this.workingWidth / this.workingHeight, 0.01, 1.0);
+
+            //for (var i = 0; i < cMesh.Vertices.length -1; i++){
+                //var point0 = this.project(cMesh.Vertices[i], transformMatrix);
+                //var point1 = this.project(cMesh.Vertices[i + 1], transformMatrix);
+                //this.drawLine(point0, point1);
+            //}
 
             for (var index = 0; index < meshes.length; index++) {
                 // current mesh to work on
@@ -81,11 +126,20 @@ var engine;
 
                 var transformMatrix = worldMatrix.multiply(viewMatrix).multiply(projectionMatrix);
 
-                for (var indexVertices = 0; indexVertices < cMesh.Vertices.length; indexVertices++) {
-                    // First, we project the 3D coordinates into the 2D space
-                    var projectedPoint = this.project(cMesh.Vertices[indexVertices], transformMatrix);
-                    // Then we can draw on screen
-                    this.drawPoint(projectedPoint);
+                for (var indexFaces = 0; indexFaces < cMesh.Faces.length; indexFaces++)
+                {
+                    var currentFace = cMesh.Faces[indexFaces];
+                    var vertexA = cMesh.Vertices[currentFace.A];
+                    var vertexB = cMesh.Vertices[currentFace.B];
+                    var vertexC = cMesh.Vertices[currentFace.C];
+
+                    var pixelA = this.project(vertexA, transformMatrix);
+                    var pixelB = this.project(vertexB, transformMatrix);
+                    var pixelC = this.project(vertexC, transformMatrix);
+
+                    this.drawBLine(pixelA, pixelB);
+                    this.drawBLine(pixelB, pixelC);
+                    this.drawBLine(pixelC, pixelA);
                 }
             }
         };
